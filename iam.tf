4f8a490beb6cf0752e94fa8a10d7612fec5d8ee7
ash16888 <ash168@yandex.ru>
2023-02-08 15:27:07 +0300

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -9,13 +9,13 @@ resource "yandex_resourcemanager_folder_iam_member" "sa-puller" {
   member    = "serviceAccount:${yandex_iam_service_account.this.id}"
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
+resource "yandex_resourcemanager_folder_iam_member" "sa-kms" {
   folder_id = var.yc_folder_id
   role      = "kms.keys.encrypterDecrypter"
   member    = "serviceAccount:${yandex_iam_service_account.this.id}"
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
   folder_id = var.yc_folder_id
   role      = "storage.admin"
   member    = "serviceAccount:${yandex_iam_service_account.this.id}"
