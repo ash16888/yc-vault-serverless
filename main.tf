7d12e44774bbde9c98f72810ed1060b53769b522
ash16888 <ash168@yandex.ru>
2023-05-31 08:15:37 +0300

    Update image url

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "yandex_serverless_container" "this" {
   core_fraction      = 100
   service_account_id = yandex_iam_service_account.this.id
   image {
-    url = "cr.yandex/crpdouee2epesrtliguv/vaultsls:1.9.4"
+    url = "cr.yandex/crpdouee2epesrtliguv/vaultsls:1.11.5"
     environment = {
       AWS_S3_BUCKET          = yandex_storage_bucket.vault.bucket
       AWS_ACCESS_KEY_ID      = yandex_iam_service_account_static_access_key.this.access_key
